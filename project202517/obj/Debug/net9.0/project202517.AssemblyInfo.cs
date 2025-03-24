88e95739bba9425bf23857dcce6bb446e3095145
Arthur Resende <arthur.resende.gomes02@gmail.com>
2025-03-23 22:29:52 -0300

    Atualização

--- project202517/obj/Debug/net9.0/project202517.AssemblyInfo.cs
+++ project202517/obj/Debug/net9.0/project202517.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("project202517")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08693d5f328cfc82897c668dca28de781c89b703")]
 [assembly: System.Reflection.AssemblyProductAttribute("project202517")]
 [assembly: System.Reflection.AssemblyTitleAttribute("project202517")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
